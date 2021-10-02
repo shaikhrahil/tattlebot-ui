3621e1b752ee4f9546aef1982554838b9e46cefe
shaikhrahil <smohammadrahil@gmail.com>
2021-10-02 19:02:40 +0530

    :sparkles: feat: added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  prefix: 'tw-',
+  purge: ['./apps/**/*.{js,ts,jsx,tsx}', './libs/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
